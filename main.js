e1f8e7b01f10cac7576fb0baa7463754cb0c3ce5
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-29 12:08:45 +0530

    Created separate functions and removed lines from main, readability improved

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 const fs = require('fs');
-const {presentWorkingDirectory, myBash} = require('./lib/bash-lib.js');
+const {myBash} = require('./lib/bash-lib.js');
 
-const main = function() {
-  const scriptFile = process.argv[2];
-  const scriptContent = fs.readFileSync(scriptFile, 'utf-8');
+const parseScript = function(file) {
+  const scriptFile = file;
+  const scriptContent = fs.readFileSync(scriptFile, 'utf-8').trim();
   const commands = scriptContent.split('\n');
-  commands.splice(-1);
+
+  return commands;
+};
+
+const execute = function(commands) {
   const outputs = [];
   let pwd = process.env.PWD;
 
   for(command of commands) {
     let result = myBash(command, pwd);
+    pwd = result.pwd;
 
-    if(result.output !== undefined) {
+    if(result.output !== '') {
       outputs.push(result.output);
     }
-
-    pwd = result.pwd;
   }
 
   return outputs;
+};
+
+const main = function() {
+  const commands = parseScript(process.argv[2]);
+  console.log(execute(commands));
 };
 
 console.log(main());
