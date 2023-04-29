5bc12582327a24430835b8793a78117454567303
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-29 22:48:26 +0530

    Pwd, ls, cd is functional

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
 const fs = require('fs');
-const {myBash} = require('./lib/bash-lib.js');
-
-const parseScript = function(file) {
-  const scriptFile = file;
-  const scriptContent = fs.readFileSync(scriptFile, 'utf-8').trim();
-  const commands = scriptContent.split('\n');
-
-  return commands;
-};
-
-const execute = function(commands) {
-  const outputs = [];
-  let pwd = process.env.PWD;
-
-  for(command of commands) {
-    let result = myBash(command, pwd);
-    pwd = result.pwd;
-
-    if(result.output !== '') {
-      outputs.push(result.output);
-    }
-  }
-
-  return outputs;
-};
+const {read, parse, execute} = require('./lib/bash-lib.js');
 
 const main = function() {
-  const commands = parseScript(process.argv[2]);
-  console.log(execute(commands));
+  const pwd = process.env.PWD;
+  const environment = {pwd};
+  const script = read(process.argv[2]);
+  const commandLines = parse(script);
+  const output = execute(commandLines, environment);
+  console.log(output);
 };
 
-console.log(main());
+main();
