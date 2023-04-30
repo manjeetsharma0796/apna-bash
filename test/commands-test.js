3aea73ec871ef36d822d9fc3640bb46c3f063ba2
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-30 16:07:52 +0530

    Added resolve, functional for removing .. from path

--- test/commands-test.js
+++ test/commands-test.js
@@ -1,6 +1,6 @@
 const {describe, it} = require('node:test');
 const {strictEqual, deepStrictEqual} = require('assert');
-const {pwd, ls, cd, echo} = require('../lib/commands.js')
+const {pwd, ls, cd, resolve, echo} = require('../lib/commands.js')
 
 describe("Testing commands", function() {
   describe("Testing pwd", function() {
@@ -16,8 +16,18 @@ describe("Testing commands", function() {
   })
 
   describe("Testing cd", function() {
-    it("Should give pwd back when provided environment", function() {
+    it("Should give modified pwd back when provided existable path", function() {
       deepStrictEqual(cd({pwd: process.env.PWD}, 'test'), '/Users/manjeet/workspace/JS/apna-bash/test');
+    })
+
+    it("Should give error message back when provided non existable path", function() {
+      deepStrictEqual(cd({pwd: process.env.PWD}, 'not-existing'), 'Directory or path does not exists');
+    })
+  })
+
+  describe("Testing resolve", function() {
+    it("Should give a path without .. when provided with path with .. included", function() {
+      deepStrictEqual(resolve('User/system/guest/../'), 'User/system/');
     })
   })
 })
